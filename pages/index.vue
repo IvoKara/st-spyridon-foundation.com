1e1b33edd4b8b42566e2469c983ae5d8f5565703
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2023-05-19 23:19:32 +0300

    Update index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <h1 font-cyrillic text-4xl>
+  <h1 text-3xl font-cyrillic md:text-4xl>
     Фондация "Св. Спиридон Тримитутски Чудотворец"
   </h1>
+  <img
+    src="/st_spyridon_ikon.jpg"
+    mx-auto mt mt-10 max-w-120 w-full rounded-4
+    drop-shadow="lg color-black/25"
+    alt="Икона на св. Спиридон"
+  >
+
+  <p mt-10 text-2xl font-alegreya>
+    Още предстои скоро...
+  </p>
 </template>
