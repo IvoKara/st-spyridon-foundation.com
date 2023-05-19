4d13c2ac3001772f84c65a43bf94e02c1ead28ad
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2023-05-19 22:08:46 +0300

    Choose main theme colors

--- app.vue
+++ app.vue
@@ -7,7 +7,7 @@ useHead({
 </script>
 
 <template>
-  <VitePwaManifest />
+  <!-- <VitePwaManifest /> -->
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
@@ -15,13 +15,10 @@ useHead({
 
 <style>
 html, body , #__nuxt{
-  height: 100vh;
-  margin: 0;
-  padding: 0;
+  --uno: min-h-screen m-0 p-0;
 }
 
-html.dark {
-  background: #222;
-  color: white;
+html {
+  --uno: bg-antique text-brick;
 }
 </style>
