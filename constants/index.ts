76785e35ca6493c37a653b6d04d70fbe1d5d68d3
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2023-05-20 17:12:06 +0300

    Donation account info

--- constants/index.ts
+++ constants/index.ts
@@ -7,3 +7,7 @@ export const appName = foundationNameShortBg
 export const appDescription = '"Св. Спиридон Тримитунтски Чудотворец" '
 + 'е фондация, чиято цел е изграждането на храм в Студентски град, София, '
 + 'на името на светеца, в помощ на ближния'
+
+export const bank = 'УниКредит Булбанк'
+export const iban = 'BG53UNCR70001524269241'
+export const bic = 'UNCRBGSF'
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
+<script setup lang="ts">
+import { bank, bic, iban } from '~/constants'
+</script>
+
 <template>
   <h1 text="3xl md:4xl" font-cyrillic>
-    Фондация <br lg:hidden>
+    Фондация <br>
     "Св. Спиридон <br md:hidden> Тримитунтски Чудотворец"
   </h1>
   <img
@@ -9,6 +13,17 @@
     drop-shadow="lg color-black/25"
     alt="Икона на св. Спиридон"
   >
+
+  <div mx-auto mt-10 w-fit text-xl font-alegreya>
+    <h4 mb-2 text-2xl font-bold>
+      Дарителска сметка
+    </h4>
+    <div text-start space-y-1>
+      <p><b>Банка: </b> {{ bank }}</p>
+      <p><b>IBAN: </b> {{ iban }}</p>
+      <p><b>BIC: </b> {{ bic }}</p>
+    </div>
+  </div>
 
   <p mt-10 text-lg font-alegreya md:text-2xl>
     Още предстои скоро...
