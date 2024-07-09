cfd41c5d9a46ce62e0a734231cf4b11fd56a709e
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2024-07-09 03:44:33 +0300

    fix: create pages to avoid build error

---
+++ components/H1.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 text="3xl md:4xl" font-bold font-cyrillic>
+    <slot />
+  </h1>
+</template>>
---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <H1>За нас</H1>
+</template>
+
+<style>
+
+</style>
